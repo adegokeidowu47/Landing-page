fd42e4f02b10ca022b3e2ff19af39f9a3f1b664f
Adegoke Idowu <adegokeidowu47@gmail.com>
2024-05-29 09:43:23 -0700

    Refractor style codes

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 const accordion = document.getElementsByClassName("questans");
+const btn = document.querySelector('#need-help img');
 
 
 for (let i = 0; i  < accordion.length; i++) {
-  accordion[i].addEventListener("click",function (){
+  accordion[i].addEventListener("click",function (e){
     this.classList.toggle("active");
+
+    // if(e.target){
+    //   this.classList.add('active');
+    // }
+    // else{
+    //   this.classList.remove('active');
+    // }
   });
 }
+
+const toggleBtn = () => {
+  btn.addEventListener("click", function () {
+    document.querySelector('#showLi').classList.toggle('show');
+  })
+}
+
+toggleBtn();

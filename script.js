3f28e0fe4af24db8d2b1740060c30d962023f1c2
Adegoke Idowu <adegokeidowu47@gmail.com>
2025-07-07 06:24:39 -0700

    New development on the landing page

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-const accordion = document.getElementsByClassName("questans");
+const accordion = document.querySelectorAll(".questans");
 const btn = document.querySelector('#need-help img');
 
 
-for (let i = 0; i  < accordion.length; i++) {
-  accordion[i].addEventListener("click",function (e){
+
+for (let i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener("click", function (e) {
     this.classList.toggle("active");
 
-    // if(e.target){
-    //   this.classList.add('active');
-    // }
-    // else{
-    //   this.classList.remove('active');
-    // }
+    if (e.target) {
+      this.classList.add('active');
+    }
+    else {
+      this.classList.remove('active');
+    }
   });
 }
 

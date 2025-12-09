79abae5bbe485bd0edb8759b1d4273034da22e1d
Adegoke Idowu <adegokeidowu47@gmail.com>
2025-12-08 16:36:27 -0800

    FIxed the accordion

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-const accordion = document.querySelectorAll(".questans");
+const headers = document.querySelectorAll(".accordion-header");
 const btn = document.querySelector('#need-help img');
 
 
+headers.forEach((header => {
+  header.addEventListener("click", () => {
+    const parent = header.parentElement;
 
-for (let i = 0; i < accordion.length; i++) {
-  accordion[i].addEventListener("click", function (e) {
-    this.classList.toggle("active");
 
-    if (e.target) {
-      this.classList.add('active');
-    }
-    else {
-      this.classList.remove('active');
-    }
+    document.querySelectorAll('.accordion-item').forEach(item => {
+
+      if (item !== parent) {
+        item.classList.remove("active");
+
+      }
+    });
+
+    parent.classList.toggle("active");
   });
-}
+}))
 
 const toggleBtn = () => {
   btn.addEventListener("click", function () {
